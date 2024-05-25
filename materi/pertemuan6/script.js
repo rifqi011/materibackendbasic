9e09ba002c78eed6fd9898ea005b7a4305ce8a6f
rifqi011 <rifqibanusafingi11@gmail.com>
2024-05-25 10:36:02 +0700

    Add pertemuan 6

---
+++ materi/pertemuan6/script.js
@@ -0,0 +1,60 @@
+// Output konsol
+console.log("Ini yg eksternal")
+
+// Variable
+const nama = "Rifqi Banu Safingi" // konstanta
+let tahun = new Date()
+let umur = tahun.getFullYear() - 2009
+
+console.log("Namaku " + nama)
+console.log("Umurku " + umur + " tahun")
+
+// Tipe Data String
+let asalSekolah = "SMK Negeri 1 Purwokerto"
+
+// Tipe Data Number
+let x = 10
+let y = 1.25
+
+// Tipe Data BigInt
+let utangNegara = 8253000000000000n
+
+// Tipe Data Boolean
+let iLoveYou = true
+let iHateYou = false
+
+//Tipe Data Undefined
+let kosong
+console.log(kosong)
+
+//Tipe Data Null
+let dosa = null
+
+//Tipe Data Object
+let identitas = {
+    nama: "Rifqi",
+    umur: umur,
+    alamat: "Bumi",
+    hidup: true
+}
+
+// Tipe Data Array
+let buah = ["Apel", "Pisang", "Nanas"]
+
+
+// Operator Aritmatika
+console.log(x+1)
+console.log(x*2)
+
+// Operator Logika
+// AND
+console.log(true && true) //true
+console.log(true && false) //false
+console.log(false && false) //false
+//OR
+console.log(true || true) //true
+console.log(true || false) //true
+console.log(false || false) //false
+//NOT
+console.log(!true) //false
+console.log(!false) //true
